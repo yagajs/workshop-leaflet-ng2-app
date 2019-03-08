60de019a4880126662c70eecbb6ca89e0ca0fc9c
Arne Schubert <atd.schubert@gmail.com>
2019-03-08 22:02:57 +0100

    Add properties to our component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,11 @@ import { MapComponent } from '@yaga/leaflet-ng2';
 export class HomePage {
   @ViewChild(MapComponent) private mapComponent: MapComponent;
 
+  public tileLayerUrl = 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
+  public mapLat = 51.03522;
+  public mapLng = 13.73427;
+  public mapZoom = 12;
+
   ionViewWillEnter() {
     this.mapComponent.invalidateSize();
   }
